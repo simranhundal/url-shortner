7118526956d5fec473f3660d7482be1559fface2
simranhundal <76602602+simranhundal@users.noreply.github.com>
2021-02-18 23:18:14 -0800

    Add files via upload

---
+++ urlshortner.py
@@ -0,0 +1,22 @@
+import requests
+# getting user input for link
+print("Enter Url to be shortened")
+url = input()
+
+# hiding my api key use provided executable file for testing
+api_key = "d1833b1413e2b9d027b12a37f1ee0745a1bd8"
+# using cuttly api with key and given url to make shortened link
+api_url = f"https://cutt.ly/api/api.php?key={api_key}&short={url}"
+
+# response is given as json 
+response = requests.get(api_url).json()["url"]
+
+# various error checking if link is not valid
+if response["status"] == 7:
+    print("Shortened URL is:  ", response["shortLink"])
+elif response["status"] == 2:
+    print("Error link is incorrect")
+elif response["status"] == 1:
+    print("Error link is already shortened")
+else:
+    print("error")
